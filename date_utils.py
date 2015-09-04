84a7bf1b8b37477f0aed0a792f9d8892dbd1d194
Tom Naessens <Tom.Naessens@UGent.be>
2015-09-04 18:14:47 +0200

    Add ui, db and actions for reminders

---
+++ date_utils.py
@@ -0,0 +1,26 @@
+"""
+This module handles easy date and time picking using timedelta, dateutil and
+relativedelta
+"""
+import inspect
+import re
+from datetime import datetime
+from time import mktime
+
+import parsedatetime
+
+
+def parse(datestring: str, default=None) -> datetime:
+    cal = parsedatetime.Calendar()
+
+    time_struct, parse_status = cal.parse(datestring)
+
+    # Parse returns 0 as parse_status if nothing is parsed
+    if parse_status:
+        return datetime.fromtimestamp(mktime(time_struct))
+    else:
+        None
+
+
+def format(dt: datetime) -> str:
+    return dt.strftime("%Y-%m-%d %H:%M")
